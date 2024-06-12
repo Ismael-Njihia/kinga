4aae2b7f753eeae3d0020c69c2fdf32fcc36e9bd
-g <-g>
2024-06-12 14:59:43 +0300

    done!!

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,24 @@ import express from "express"
 import dotenv from "dotenv"
 import bookingRoute from "./Routes/bookingRoute.js"
 import cors from "cors"
+import path from "path"
+import { fileURLToPath } from 'url';
 
 const app = express()
 dotenv.config()
 app.use(express.json())
 app.use(cors())
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use("/api/booking", bookingRoute)
+app.use(express.static(path.join(__dirname, '..')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000
 
