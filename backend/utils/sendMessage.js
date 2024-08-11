39d0c773550fb43daf2171652718806431207584
-g <-g>
2024-08-11 03:36:09 -0400

    changes on the booking part

--- backend/utils/sendMessage.js
+++ backend/utils/sendMessage.js
@@ -1,9 +1,17 @@
 import nodemailer from 'nodemailer';
 export const sendMessage = async({fullName, phoneNumber, day, time, Message}) => {
     console.log(fullName, "Full Name");
+    console.log(phoneNumber, "Phone Number");
+    console.log(day, "Day");
+    console.log(time, "Time");
+
+
     try {
         const EMAIL = process.env.EMAIL;
         const PASSWORD = process.env.PASSWORD;
+
+        const to = "ishmaelnjihia@gmail.com";
+        console.log(to, "To");
 
         // Create a transporter
         const transporter = nodemailer.createTransport({
@@ -14,12 +22,19 @@ export const sendMessage = async({fullName, phoneNumber, day, time, Message}) =>
             }
         });
 
-        // Email options
-        const mailOptions = {
-            from: EMAIL,
-            to: 'kavunyefransisca@gmail.com',
-            subject: 'New Booking from Your Website',
-            html: `
+// Email options
+const mailOptions = {
+    from: EMAIL,
+    to: 'ishmaelnjihia@gmail.com',
+
+    // to: 'kavunyefransisca@gmail.com',
+    subject: 'New Booking from Your Website',
+    html: `
+        <div style="background-color: #FF6347; padding: 20px 0;">
+            <div style="background-color: #182366; color: white; text-align: center; padding: 10px 0;">
+                <h1>Kinga Ceragem! Good News</h1>
+            </div>
+            <div style="background-color: white; padding: 20px; margin: 20px;">
                 <p>Hi Kinga Ceragem!,</p>
                 <p>You have a new booking from your website</p>
                 <h3>Booking Details</h3>
@@ -30,8 +45,13 @@ export const sendMessage = async({fullName, phoneNumber, day, time, Message}) =>
                     <li>Time: ${time}</li>
                     <li>Message: ${Message}</li>
                 </ul>
-            `
-        };
+                <p>Thank you</p>
+                <pThis email has been sent to: ${EMAIL}</p>
+            </div>
+        </div>
+    `
+};
+
 
         // Send email
         const info = await transporter.sendMail(mailOptions);
