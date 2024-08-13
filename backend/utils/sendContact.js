f3810dfdb1b870088e2cfa3329cb4a49356c0242
-g <-g>
2024-08-13 01:15:24 -0400

    changed the format

--- backend/utils/sendContact.js
+++ backend/utils/sendContact.js
@@ -6,6 +6,9 @@ export const sendContact = async ({ fullName, email, topic, phone, message }) =>
         const EMAIL = process.env.EMAIL;
         const PASSWORD = process.env.PASSWORD;
 
+        const to = "kavunyefransisca@gmail.com";
+        console.log(to, "To");
+
         // Create a transporter
         const transporter = nodemailer.createTransport({
             service: 'gmail',
@@ -18,20 +21,30 @@ export const sendContact = async ({ fullName, email, topic, phone, message }) =>
         // Email options
         const mailOptions = {
             from: EMAIL,
-            to: 'kavunyefransisca@gmail.com',
+            to: to,
             subject: topic,
             html: `
-                <p>Hi Kinga Ceragem!,</p>
-                <p>You have a new contact from your website</p>
-                <h3>Contact Details</h3>
-                <ul>
-                    <li>Full Name: ${fullName}</li>
-                    <li>Email: ${email}</li>
-                    <li>Phone: ${phone}</li>
-                    <li>Message: ${message}</li>
-                </ul>
+                <div style="background-color: #FF6347; padding: 20px 0;">
+                    <div style="background-color: #182366; color: white; text-align: center; padding: 10px 0;">
+                        <h1>Kinga Ceragem! Good News</h1>
+                    </div>
+                    <div style="background-color: white; padding: 20px; margin: 20px;">
+                        <p>Hi Kinga Ceragem!,</p>
+                        <p>You have a new contact from your website</p>
+                        <h3>Contact Details</h3>
+                        <ul>
+                            <li>Full Name: ${fullName}</li>
+                            <li>Email: ${email}</li>
+                            <li>Phone: ${phone}</li>
+                            <li>Message: ${message}</li>
+                        </ul>
+                        <p>Thank you</p>
+                        <p>This email has been sent to: ${EMAIL}</p>
+                    </div>
+                </div>
             `
         };
+        
         const info = await transporter.sendMail(mailOptions);
         console.log('Message sent: %s', info.messageId);
         return info;
